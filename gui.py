01c0879d912c428d4805923d338bb35e8030c392
Pinkos1 <Pinkos2003@gmail.com>
2026-03-29 11:40:48 -0400

    add buttons

--- gui.py
+++ gui.py
@@ -48,4 +48,81 @@ class VideoPlayerGUI:
             width = 12
         )
 
+        self.open_button.pack(side = tk.LEFT, padx = 5)
+
+
+
+        self.play_button = tk.Button(
+            top_frame,
+            text = "Play",
+            command = self.play_video,
+            width = 10
+        )
+        self.play_button.pack(side = tk.LEFT, padx = 5)
+
+
+
+        self.pause_button = tk.Button(
+            top_frame,
+            text = "Pause",
+            command = self.pause_video,
+            width = 10
+        )
+        self.pause_button.pack(side = tk.LEFT, padx = 5)
+
+
+
+        self.stop_button = tk.Button(
+            top_frame,
+            text = "Stop",
+            command = self.stop_video,
+            width = 10
+        )
+        self.stop_button.pack(side = tk.LEFT, padx = 5)
+
+
+
+        self.restart_button = tk.Button(
+            top_frame,
+            text = "Restart",
+            command = self.restart_video,
+            width = 10
+        )
+        self.restart_button.pack(side = tk.LEFT, padx = 5)
+
+
+
+        self.people_button = tk.Button(
+            top_frame,
+            text = "Players: ON",
+            command = self.toggle_people_detection,
+            width = 12
+        )
+        self.people_button.pack(side = tk.LEFT, padx = 5)
+
+
+
+        self.rim_button = tk.Button(
+            top_frame,
+            text = "Rim: ON",
+            command = self.toggle_rim_detection,
+            width = 10
+        )
+        self.rim_button.pack(side = tk.LEFT, padx = 5)
+
+
+
+        self.label = tk.Label(
+            top_frame,
+            text = "No video selected",
+            anchor = "w"
+        )
+        self.label.pack(side = tk.LEFT, padx = 10)
+
+
+
+        self.video_label = tk.Label(self.root, bg = "black")
+        self.video_label.pack(fill=tk.BOTH, expand = True, padx = 10, pady = 10)
+
+
     
