8845f56e0187c7bef9e9a028abdfd9111abb4e5a
Pinkos1 <Pinkos2003@gmail.com>
2026-03-29 13:51:26 -0400

    added imports and track

--- person_detector.py
+++ person_detector.py
@@ -5,8 +5,13 @@ Date: March 22nd, 2026
 Brief: File detects players on the floor
 
 '''
-
+from ultralytics import YOLO
+import cv2
 
 class PersonDetector:
     def __init__(self):
         self.model = YOLO("yolov8n.pt")
+    
+
+    def detect_and_track(self, frame):
+        results = self.model.track(frame, persist = True, verbose = False)
