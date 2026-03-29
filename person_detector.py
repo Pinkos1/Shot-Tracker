86899abeb763133152933c1db99122b196be6dbf
Pinkos1 <Pinkos2003@gmail.com>
2026-03-29 11:19:40 -0400

    files for detecting

---
+++ person_detector.py
@@ -0,0 +1,7 @@
+
+'''
+Author: Adam Pinkos
+Date: March 22nd, 2026
+Brief: File detects players on the floor
+
+'''
---
+++ rim_detector.py
@@ -0,0 +1,9 @@
+
+
+'''
+Author: Adam Pinkos
+Date: March 22nd, 2026
+Brief: File detects players on the floor
+
+'''
+
