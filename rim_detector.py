e5e3436c755e74b4b68546abc2bb2db063438b46
Pinkos1 <Pinkos2003@gmail.com>
2026-03-29 14:27:31 -0400

    added imports and frame

--- rim_detector.py
+++ rim_detector.py
@@ -7,7 +7,21 @@ Brief: File detects basketball rims
 
 '''
 
+from ultralytics import YOLO
+import cv2
 
 
 class RimDetector:
 
+    def __init__(self):
+        self.model = YOLO("best_rim.pt")
+        print("Rim model loaded.")
+        print("Class names:", self.model.names)
+
+    def detect_rims(self, frame):
+        results = self.model.predict(
+            source = frame,
+            conf = 0.05,
+            iou = 0.30,
+            verbose = False
+        )
