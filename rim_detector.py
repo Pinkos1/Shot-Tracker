bf8dce373c6d6de480263ad176f7cb124f8bc4b9
Pinkos1 <Pinkos2003@gmail.com>
2026-03-29 14:53:35 -0400

    added boxes

--- rim_detector.py
+++ rim_detector.py
@@ -3,16 +3,17 @@
 '''
 Author: Adam Pinkos
 Date: March 22nd, 2026
-Brief: File detects basketball rims
+Brief: File detects players on the floor
 
 '''
+
+
 
 from ultralytics import YOLO
 import cv2
 
 
 class RimDetector:
-
     def __init__(self):
         self.model = YOLO("best_rim.pt")
         print("Rim model loaded.")
@@ -20,12 +21,11 @@ class RimDetector:
 
     def detect_rims(self, frame):
         results = self.model.predict(
-            source = frame,
-            conf = 0.05,
-            iou = 0.30,
-            verbose = False
+            source=frame,
+            conf=0.05,
+            iou=0.30,
+            verbose=False
         )
-
 
         best_rim = None
         best_conf = 0.0
@@ -49,3 +49,22 @@ class RimDetector:
             return [best_rim]
 
         return []
+
+    def draw_rims(self, frame, rims):
+        for rim in rims:
+            x1, y1, x2, y2 = rim["box"]
+            conf = rim["conf"]
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 165, 255), 3)
+
+            cv2.putText(
+                frame,
+                f"Rim {conf:.2f}",
+                (x1, max(25, y1 - 10)),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.8,
+                (0, 165, 255),
+                2
+            )
+
+        return frame
