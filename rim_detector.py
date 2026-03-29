df09fbae08890274dd24810d72513ca52c5efd5f
Pinkos1 <Pinkos2003@gmail.com>
2026-03-29 14:08:00 -0400

    rim detector class

--- rim_detector.py
+++ rim_detector.py
@@ -7,16 +7,7 @@ Brief: File detects basketball rims
 
 '''
 
-class RimDetector:
-    def __init__(self):
-        self.model = YOLO("best_rim.pt")
-        print("Rim model loaded.")
-        print("Class names:", self.model.names)
 
-    def detect_rims(self, frame):
-        results = self.model.predict(
-            source = frame,
-            conf = 0.05,
-            iou = 0.30,
-            verbose = False
-        )
+
+class RimDetector:
+
