c37333eac8c1e5cce5b143a505c3068cdec5be3f
Pinkos1 <Pinkos2003@gmail.com>
2026-03-29 14:56:51 -0400

    load video

--- video_manager.py
+++ video_manager.py
@@ -16,4 +16,25 @@ class VideoManager:
         self.is_paused = False
         self.current_frame = None
         self.fps = 30
+
+
+    def load_video(self, file_path):
+        self.release()
+
+        self.cap = cv2.VideoCapture(file_path)
+        if not self.cap.isOpened():
+            return False
+
+        self.video_path = file_path
+
+        fps = self.cap.get(cv2.CAP_PROP_FPS)
+        if fps and fps > 0:
+            self.fps = fps
+        else:
+            self.fps = 30
+
+        self.is_playing = False
+        self.is_paused = False
+        self.current_frame = None
+        return True
     
