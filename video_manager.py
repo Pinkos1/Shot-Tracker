2b6d92eed3c8d1051ba0591648b1fc46868fdd13
Pinkos1 <Pinkos2003@gmail.com>
2026-03-29 14:56:05 -0400

    added video info

--- video_manager.py
+++ video_manager.py
@@ -9,4 +9,11 @@ import cv2
 
 
 class VideoManager:
+    def __init__(self):
+        self.cap = None
+        self.video_path = None
+        self.is_playing = False
+        self.is_paused = False
+        self.current_frame = None
+        self.fps = 30
     
