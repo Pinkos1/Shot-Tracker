3afdf6c2460e9305b24b4d07b8546c70e81d1301
Pinkos1 <Pinkos2003@gmail.com>
2026-03-29 11:16:00 -0400

    add gui

---
+++ gui.py
@@ -0,0 +1,14 @@
+
+'''
+Author: Adam Pinkos
+Date: March 22nd, 2026
+Brief: gui for the program, allows to select video, pause, play and choose what is being detected. 
+'''
+
+
+import os
+import cv2
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk
+
