c826e4174cc384a73d8d03cdf46675a76bdb28bc
Pinkos1 <Pinkos2003@gmail.com>
2026-03-29 14:50:01 -0400

    main to run program

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ Date: March 22nd, 2026
 Brief: Simple main file to run the program
 
 '''
+
+import tkinter as tk
+from gui import VideoPlayerGUI
+
+
+def main():
+    root = tk.Tk()
+    app = VideoPlayerGUI(root)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
