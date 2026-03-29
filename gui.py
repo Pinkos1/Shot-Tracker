a87bc71e8a842a2a3192861d62d70bb3511d9687
Pinkos1 <Pinkos2003@gmail.com>
2026-03-29 11:28:59 -0400

    added widgets

--- gui.py
+++ gui.py
@@ -21,3 +21,31 @@ class VideoPlayerGUI:
         self.root = root
         self.root.title("Basketball Shot Tracker - Video Player")
         self.root.geometry("1100x750")
+
+
+
+        self.video_manager = VideoManager()
+        self.person_detector = PersonDetector()
+        self.rim_detector = RimDetector()
+
+        self.photo = None
+        self.detect_people_enabled = True
+        self.detect_rims_enabled = True
+        self.is_processing_frame = False
+
+        self.create_widgets()
+        self.update_loop()
+
+
+    def create_widgets(self):
+        top_frame = tk.Frame(self.root)
+        top_frame.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
+
+        self.open_button = tk.Button(
+            top_frame,
+            text="Open Video",
+            command=self.open_video,
+            width=12
+        )
+
+    
